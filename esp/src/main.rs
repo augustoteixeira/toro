5d6fa1a8df66d67a9c080e11ba5f47438bafee53
augusto <augusto@impa.br>
2026-04-01 09:10:48 -0300

    Extract async fn run() from inline block_on closure
    Move the wait-and-print logic into a named async function driven by
    block_on, preparing for future concurrent tasks via join/select.
    Document that embassy-executor is not compatible with esp-idf's
    FreeRTOS runtime.

--- esp/src/main.rs
+++ esp/src/main.rs
@@ -1,3 +1,11 @@
+use embassy_time::{Duration, Timer};
+
+async fn run() {
+    Timer::after(Duration::from_millis(100)).await;
+    log::info!("Hello, world!");
+    log::info!("BOOT_OK");
+}
+
 fn main() {
     // It is necessary to call this function once. Otherwise, some patches to the runtime
     // implemented by esp-idf-sys might not link properly. See https://github.com/esp-rs/esp-idf-template/issues/71
@@ -6,9 +14,5 @@ fn main() {
     // Bind the log crate to the ESP Logging facilities
     esp_idf_svc::log::EspLogger::initialize_default();
 
-    esp_idf_svc::hal::task::block_on(async {
-        embassy_time::Timer::after(embassy_time::Duration::from_millis(100)).await;
-        log::info!("Hello, world!");
-        log::info!("BOOT_OK");
-    });
+    esp_idf_svc::hal::task::block_on(run());
 }
