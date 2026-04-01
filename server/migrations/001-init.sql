98703775c99b7a226b14d22a9e84792b6dd516fe
augusto <augusto@impa.br>
2026-04-01 18:44:38 -0300

    Add hourly_readings table and fake dump with 3 days of data

--- server/migrations/001-init.sql
+++ server/migrations/001-init.sql
@@ -1,1 +1,11 @@
+CREATE TABLE hourly_readings (
+    hour           TEXT PRIMARY KEY,
+    temperature    REAL,
+    humidity       REAL,
+    wind_speed     REAL,
+    wind_direction REAL,
+    luminosity     REAL,
+    rainfall       REAL
+);
+
 INSERT INTO meta (key, value) VALUES ('schema_version', '1');
